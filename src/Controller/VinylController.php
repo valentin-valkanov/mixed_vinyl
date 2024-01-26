21d180810c87f4d7ec1f0978334086aedda099f7
Valentin Valkanov <valentinvalkanof@gmail.com>
2024-01-26 22:24:37 +0200

    assets-css

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -31,12 +31,11 @@ class VinylController extends AbstractController
     #[Route('/browse/{slug}')]
     public function browse(string $slug = null): Response
     {
-        if ($slug) {
-            $title = 'Genre: '.u(str_replace('-', ' ', $slug))->title(true);
-        } else {
-            $title = 'All Genres';
-        }
-        return new Response($title);
+        $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
+
+        return $this->render('vinyl/browse.html.twig', [
+            'genre' => $genre
+        ]);
     }
 
 }
