75af82c00428db2ea4bb7cf9d6a454932fe82207
Valentin Valkanov <valentinvalkanof@gmail.com>
2024-01-26 12:57:20 +0200

    profiler

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -21,6 +21,7 @@ class VinylController extends AbstractController
             ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
             ['song' => 'Fantasy', 'artist' => 'Mariah Carey']
         ];
+
         return $this->render('vinyl/homepage.html.twig', [
             'title' => 'PB & James',
             'tracks'=> $tracks,
