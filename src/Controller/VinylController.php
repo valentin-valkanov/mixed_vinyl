4370fb47108ea2b420ca1868f7fd9dff4245abf1
Valentin Valkanov <valentinvalkanof@gmail.com>
2024-01-26 07:10:00 +0200

    Symfony Flex, Flex Recipies, and Twig

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -3,16 +3,28 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('Title: PB and Jams');
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey']
+        ];
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & James',
+            'tracks'=> $tracks,
+        ]);
     }
 
     #[Route('/browse/{slug}')]
