f7febcfee10314f046b7813296ab9440d783c499
Valentin Valkanov <valentinvalkanof@gmail.com>
2024-03-22 22:22:09 +0200

    bundles

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -10,6 +10,30 @@ use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
 {
+    private function getMixes(): array
+    {
+        // temporary fake "mixes" data
+        return [
+            [
+                'title' => 'PB & Jams',
+                'trackCount' => 14,
+                'genre' => 'Rock',
+                'createdAt' => new \DateTime('2021-10-02'),
+            ],
+            [
+                'title' => 'Put a Hex on your Ex',
+                'trackCount' => 8,
+                'genre' => 'Heavy Metal',
+                'createdAt' => new \DateTime('2022-04-28'),
+            ],
+            [
+                'title' => 'Spice Grills - Summer Tunes',
+                'trackCount' => 10,
+                'genre' => 'Pop',
+                'createdAt' => new \DateTime('2019-06-20'),
+            ],
+        ];
+    }
     #[Route('/', name: 'app_homepage')]
     public function homepage(): Response
     {
@@ -32,9 +56,10 @@ class VinylController extends AbstractController
     public function browse(string $slug = null): Response
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
-
+        $mixes = $this->getMixes();
         return $this->render('vinyl/browse.html.twig', [
-            'genre' => $genre
+            'genre' => $genre,
+            'mixes' => $mixes,
         ]);
     }
 
