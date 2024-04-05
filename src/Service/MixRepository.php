9ad537eecf8c7854b4585a2b5f73fd09cc7ff711
Valentin Valkanov <valentinvalkanof@gmail.com>
2024-04-05 23:21:25 +0300

    non-autowirebale services

--- src/Service/MixRepository.php
+++ src/Service/MixRepository.php
@@ -1,29 +1,31 @@
 <?php
-declare(strict_types=1);
-
 namespace App\Service;
-
 use Psr\Cache\CacheItemInterface;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Bridge\Twig\Command\DebugCommand;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Contracts\Cache\CacheInterface;
-
-
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Twig\Loader\FilesystemLoader;
 class MixRepository
 {
     public function __construct(
-        private HttpClientInterface $httpClient,
+        private HttpClientInterface $githubContentClient,
         private CacheInterface $cache,
-        private bool $isDebug
-    )
-    {
-    }
-
+        #[Autowire('%kernel.debug%')]
+        private bool $isDebug,
+        #[Autowire(service: 'twig.command.debug')]
+        private DebugCommand $twigDebugCommand,
+    ) {}
     public function findAll(): array
     {
+//        $output = new BufferedOutput();
+//        $this->twigDebugCommand->run(new ArrayInput([]), $output);
+//        dd($output);
         return $this->cache->get('mixes_data', function(CacheItemInterface $cacheItem) {
-            $cacheItem->expiresAfter($this->isDebug ? 5: 60);
-            $response = $this->httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
-
+            $cacheItem->expiresAfter($this->isDebug ? 5 : 60);
+            $response = $this->githubContentClient->request('GET', '/SymfonyCasts/vinyl-mixes/main/mixes.json');
             return $response->toArray();
         });
     }
