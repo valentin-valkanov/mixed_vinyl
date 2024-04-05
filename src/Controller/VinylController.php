7c9bbd7e7c80b431db92e431509f0ad29c29a677
Valentin Valkanov <valentinvalkanof@gmail.com>
2024-04-05 23:34:47 +0300

    controllers-are-services-tool

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,18 +2,18 @@
 declare(strict_types=1);
 
 namespace App\Controller;
-
 use App\Service\MixRepository;
-use Psr\Cache\CacheItemInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Cache\CacheInterface;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 use function Symfony\Component\String\u;
-
 class VinylController extends AbstractController
 {
+    public function __construct(
+        private bool $isDebug,
+        private MixRepository $mixRepository
+    )
+    {}
     #[Route('/', name: 'app_homepage')]
     public function homepage(): Response
     {
@@ -23,26 +23,22 @@ class VinylController extends AbstractController
             ['song' => 'Creep', 'artist' => 'Radiohead'],
             ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
             ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
-            ['song' => 'Fantasy', 'artist' => 'Mariah Carey']
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
         ];
-
         return $this->render('vinyl/homepage.html.twig', [
             'title' => 'PB & Jams',
-            'tracks'=> $tracks,
+            'tracks' => $tracks,
         ]);
     }
-
     #[Route('/browse/{slug}', name: 'app_browse')]
-    public function browse(MixRepository $mixRepository, string $slug = null): Response
+    public function browse(string $slug = null): Response
     {
+        dump($this->isDebug);
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
-        $mixes = $mixRepository->findAll();
-
-
+        $mixes = $this->mixRepository->findAll();
         return $this->render('vinyl/browse.html.twig', [
             'genre' => $genre,
             'mixes' => $mixes,
         ]);
     }
-
 }
