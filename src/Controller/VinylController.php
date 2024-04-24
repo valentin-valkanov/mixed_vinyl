f9ec6cc230c9e879680a2114e712e9e845902b40
Valentin Valkanov <valentinvalkanof@gmail.com>
2024-04-24 23:18:39 +0300

    Querying the Database

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,7 +2,10 @@
 declare(strict_types=1);
 
 namespace App\Controller;
+use App\Entity\VinylMix;
+use App\Repository\VinylMixRepository;
 use App\Service\MixRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -31,11 +34,14 @@ class VinylController extends AbstractController
         ]);
     }
     #[Route('/browse/{slug}', name: 'app_browse')]
-    public function browse(string $slug = null): Response
+    public function browse(VinylMixRepository $mixRepository, string $slug = null): Response
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
-        $mixes = $this->mixRepository->findAll();
+        $mixes = $mixRepository->findAll();
+
+
         return $this->render('vinyl/browse.html.twig', [
+
             'genre' => $genre,
             'mixes' => $mixes,
         ]);
