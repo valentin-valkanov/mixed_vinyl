03a0b28118d7d6b5404a556b86fb29e1155e308b
Valentin Valkanov <valentinvalkanof@gmail.com>
2024-04-27 15:37:59 +0300

    updating-entity

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -42,14 +42,21 @@ class MixController extends AbstractController
     }
 
     #[Route('/mix/{id}/vote', name: 'app_mix_vote', methods: ['POST'])]
-    public function vote(VinylMix $mix, Request $request): Response
+    public function vote(VinylMix $mix, Request $request, EntityManagerInterface $entityManager): Response
     {
          $direction = $request->request->get('direction', 'up');
          if($direction === 'up'){
-             $mix->setVotes($mix->getVotes() + 1);
+             $mix->upVote();
          }else{
-             $mix->setVotes($mix->getVotes() - 1);
+             $mix->downVote();
          }
-         dd($mix);
+
+         $entityManager->flush();
+
+         $this->addFlash('success', "Vote counted!");
+
+         return $this->redirectToRoute('app_mix_show', [
+             'id' => $mix->getId()
+         ]);
     }
 }
