f063f09aa8d821feb779cc331423c74e49158cdd
Valentin Valkanov <valentinvalkanof@gmail.com>
2024-04-28 23:44:25 +0300

    20-foundry-to-22-turbo-frames

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -6,7 +6,10 @@ use App\Entity\VinylMix;
 use App\Repository\VinylMixRepository;
 use App\Service\MixRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Pagerfanta\Doctrine\ORM\QueryAdapter;
+use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
@@ -34,17 +37,19 @@ class VinylController extends AbstractController
         ]);
     }
     #[Route('/browse/{slug}', name: 'app_browse')]
-    public function browse(VinylMixRepository $mixRepository, string $slug = null): Response
+    public function browse(VinylMixRepository $mixRepository, Request $request, string $slug = null): Response
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
-
-        $mixes = $mixRepository->findAllOrderedByVotes($slug);
-
-
+        $queryBuilder = $mixRepository->createOrderedByVotesQueryBuilder($slug);
+        $adapter = new QueryAdapter($queryBuilder);
+        $pagerfanta = Pagerfanta::createForCurrentPageWithMaxPerPage(
+            $adapter,
+            $request->query->getInt('page', 1),
+            9
+        );
         return $this->render('vinyl/browse.html.twig', [
-
             'genre' => $genre,
-            'mixes' => $mixes,
+            'pager' => $pagerfanta,
         ]);
     }
 }
--- src/Repository/VinylMixRepository.php
+++ src/Repository/VinylMixRepository.php
@@ -22,10 +22,7 @@ class VinylMixRepository extends ServiceEntityRepository
         parent::__construct($registry, VinylMix::class);
     }
 
-        /**
-         * @return VinylMix[] Returns an array of VinylMix objects
-         */
-        public function findAllOrderedByVotes(string $genre = null): array
+        public function createOrderedByVotesQueryBuilder(string $genre = null): QueryBuilder
         {
             $queryBuilder =  $this->addOrderByVotesQueryBuilder();
 
@@ -33,9 +30,7 @@ class VinylMixRepository extends ServiceEntityRepository
                 $queryBuilder->andWhere('mix.genre = :genre')
                     ->setParameter('genre', $genre);
             }
-            return $queryBuilder
-                ->getQuery()
-                ->getResult();
+            return $queryBuilder;
         }
 
         private function addOrderByVotesQueryBuilder(QueryBuilder $queryBuilder = null): QueryBuilder
